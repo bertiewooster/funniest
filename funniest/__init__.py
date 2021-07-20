e97e7fbcc8a5ff79acd79859221d310df35542c8
bertiewooster <jemonat@calalum.org>
2021-07-19 23:13:27 -0400

    Restored joke() to text.py (only)

--- funniest/__init__.py
+++ funniest/__init__.py
@@ -1,4 +1,1 @@
-#from .text import joke
-def joke():
-    return (u'text.py: Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+from .text import joke
--- funniest/text.py
+++ funniest/text.py
@@ -1,3 +1,3 @@
-def joke_text():
+def joke():
     return (u'text.py: Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
             u'Beiherhund das Oder die Flipperwaldt gersput.')
